01f3d11a94c043daf9aed1462d7a8629202e8532
gashach <gashach@users.noreply.github.com>
2021-11-23 19:56:56 -0500

    add getAllOrgs

---
+++ EncompassSettings/EncompassSettingsManager/Orgs.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace EncompassSettings.EncompassSettingsManager
+{
+    public static class Orgs
+    {
+        public static string GetAllOrgs(this EncompassSessionManager manager)
+        {
+            var orgs = manager.EncompassDefaultInstance.OrganizationManager.GetAllOrganizations();
+            return JsonConvert.SerializeObject(orgs);
+        }
+    }
+}
