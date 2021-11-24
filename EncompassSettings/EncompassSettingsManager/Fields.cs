234c0bf8e429d7dcb6529f7341e0de4561652ea9
gashach <gashach@users.noreply.github.com>
2021-11-23 19:43:37 -0500

    add piggyback fields need to finish reports

---
+++ EncompassSettings/EncompassSettingsManager/Fields.cs
@@ -0,0 +1,14 @@
+﻿using EllieMae.EMLite.DataEngine;
+using Newtonsoft.Json;
+
+namespace EncompassSettings.EncompassSettingsManager
+{
+    public static class Fields
+    {
+        public static string GetAllPiggybackFields(this EncompassSessionManager manager)
+        {
+            var pfFields = (PiggybackFields)manager.EncompassDefaultInstance.GetSystemSettings(typeof(PiggybackFields));
+            return JsonConvert.SerializeObject(pfFields.GetSyncFields());
+        }
+    }
+}
---
+++ EncompassSettings/EncompassSettingsManager/Reports.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using EllieMae.EMLite.Common;
+
+namespace EncompassSettings.EncompassSettingsManager
+{
+    public static class Reports
+    {
+        public static string GetAllPublicReports(this EncompassSessionManager manager)
+        {
+            var reportMainList = manager.EncompassSessionObjects.ReportManager.GetAllPublicReportDirEntries().ToList();
+            var reportList = reportMainList.Where(x => x.Type == FileSystemEntry.Types.File).ToList();
+            foreach (var entry in  reportList)
+            {
+                var report = entry;
+                if (report.Path.Contains(("%2A")))
+                {
+                    report = new FileSystemEntry(report.ParentFolder.Path.Replace("%2A", "*"), report.Name, report.Type,
+                        report.Owner);
+                }
+
+                var settings = manager.EncompassSessionObjects.ReportManager.GetReportSettings(report);
+                
+            }
+        }
+    }
+}
