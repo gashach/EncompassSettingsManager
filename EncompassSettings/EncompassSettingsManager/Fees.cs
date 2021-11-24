92dc63cd7dc6ad997f238dfa8160c2dd05259f17
gashach <gashach@users.noreply.github.com>
2021-11-23 19:35:12 -0500

    add GetAllFees

---
+++ EncompassSettings/EncompassSettingsManager/Fees.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace EncompassSettings.EncompassSettingsManager
+{
+    public static class Fees
+    {
+        public static string GetAllFees(this EncompassSessionManager manager)
+        {
+            var fees = manager.EncompassSessionObjects.ConfigurationManager.GetFeeManagement();
+            return JsonConvert.SerializeObject(fees);
+        }
+    }
+}
